9cd6682d8a4bb4111816340236b89df15ac9353b
Michaela Drummonds <mickeysymone@gmail.com>
2022-09-02 14:37:07 -0400

    added methods to get all businesses/ individuals

--- src/main/java/com/banking/springboot/service/CustomerService.java
+++ src/main/java/com/banking/springboot/service/CustomerService.java
@@ -4,14 +4,24 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.banking.springboot.model.Business;
 import com.banking.springboot.model.Customer;
+import com.banking.springboot.model.Individual;
 
 @Component
 public interface CustomerService {
-	
+
 	List<Customer> getAllCustomers();
+
 	Customer getCustomerById(Long id);
+
 	Customer saveCustomer(Customer customer);
+
 	void deleteCustomerById(Long id);
+
 	Customer updateCustomer(Customer customer);
+
+	List<Individual> getAllIndividuals();
+
+	List<Business> getAllBusinesses();
 }
